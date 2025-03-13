18cc13b7cea67b733d795da59d7b0205496cc628
rupesh3433 <poudelrupace@gmail.com>
2025-03-13 13:23:36 +0545

    model deployed for anamoly detection

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +11,27 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      // Proxy endpoints to your backend
+      '/upload': {
+        target: 'https://apachelogparser-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/download': {
+        target: 'https://apachelogparser-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/predict': {
+        target: 'https://apachelogparser-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 3000, // Increase the warning limit (in KB) as needed
+  },
+});
